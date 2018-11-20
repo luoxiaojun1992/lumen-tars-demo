0602f11383b458a1bcb99515443cdfe383070375
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-20 17:50:43 +0800

    merge config

--- src/config/tars.php
+++ src/config/tars.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    //http服务网关
     'registries' => [
         [
             'type' => 'kong',
@@ -13,4 +14,36 @@ return [
     'log_level' => \Monolog\Logger::INFO,
 
     'communicator_config_log_level' => 'INFO',
+
+    'services' => [
+        'namespaceName' => 'Lxj\Laravel\Tars\\',
+        'monitorStoreConf' => [
+            //'className' => Tars\monitor\cache\RedisStoreCache::class,
+            //'config' => [
+            // 'host' => '127.0.0.1',
+            // 'port' => 6379,
+            // 'password' => ':'
+            //],
+            'className' => Tars\monitor\cache\SwooleTableStoreCache::class,
+            'config' => [
+                'size' => 40960
+            ]
+        ],
+
+//    'home-api' => '\App\Tars\servant\PHPTest\PHPServer\obj\TestTafServiceServant', //根据实际情况替换，遵循PSR-4即可，与tars.proto.php配置一致
+//    'home-class' => '\App\Tars\impl\TestTafServiceImpl', //根据实际情况替换，遵循PSR-4即可
+    ],
+
+    'proto' => [
+        'appName' => 'PHPTest', //根据实际情况替换
+        'serverName' => 'PHPHTTPServer', //根据实际情况替换
+        'objName' => 'obj', //根据实际情况替换
+//    'withServant' => true, //决定是服务端,还是客户端的自动生成,true为服务端
+//    'tarsFiles' => array(
+//        //根据实际情况填写
+//        './example.tars',
+//    ),
+//    'dstPath' => '../src/app/Tars/servant', //可替换，遵循PSR-4规则
+//    'namespacePrefix' => 'App\Tars\servant', //可替换，遵循PSR-4规则
+    ],
 ];
--- tars/tars.proto.php
+++ tars/tars.proto.php
@@ -1,13 +1,3 @@
 <?php
-return array(
-    'appName' => 'PHPTest', //根据实际情况替换
-    'serverName' => 'PHPHTTPServer', //根据实际情况替换
-    'objName' => 'obj', //根据实际情况替换
-//    'withServant' => true, //决定是服务端,还是客户端的自动生成,true为服务端
-//    'tarsFiles' => array(
-//        //根据实际情况填写
-//        './example.tars',
-//    ),
-//    'dstPath' => '../src/app/Tars/servant', //可替换，遵循PSR-4规则
-//    'namespacePrefix' => 'App\Tars\servant', //可替换，遵循PSR-4规则
-);
+$tarsConfig = include __DIR__ . '/../src/config/tars.php';
+return $tarsConfig['proto'];
