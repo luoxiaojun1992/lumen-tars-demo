5cafd13e0eeee1e19ce1b1ea9e742b31d310294b
luoxiaojun <luoxiaojun@dodoca.com>
2019-11-15 10:39:16 +0800

    custom route

--- src/config/tars.php
+++ src/config/tars.php
@@ -40,7 +40,7 @@ return [
     'proto' => [
         'appName' => 'PHPTest', //根据实际情况替换
         'serverName' => 'LumenTars', //根据实际情况替换
-        'objName' => 'tarsObj', //根据实际情况替换
+        'objName' => 'obj', //根据实际情况替换
         'withServant' => true, //决定是服务端,还是客户端的自动生成,true为服务端
         'tarsFiles' => array(
             //根据实际情况填写
--- src/routes/web.php
+++ src/routes/web.php
@@ -15,23 +15,21 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => '/Laravel/route'], function () use ($router) {
-    $router->group(['prefix' => '/test'], function () use ($router) {
-        $router->get('/http', function () {
-            \Illuminate\Support\Facades\Log::info('test lumen tars log');
-            return app('service.demo')->ping() .
-                ':接入Lumen Router成功啦,配置:' . json_encode(config('foo')) . ',入参:' .
-                json_encode(app('request')->all());
-        });
+$router->group(['prefix' => '/test'], function () use ($router) {
+    $router->get('/http', function () {
+        \Illuminate\Support\Facades\Log::info('test lumen tars log');
+        return app('service.demo')->ping() .
+            ':接入Lumen Router成功啦,配置:' . json_encode(config('foo')) . ',入参:' .
+            json_encode(app('request')->all());
+    });
 
-        $router->get('/tars', function () {
-            try {
-                $config = \Lxj\Laravel\Tars\Config::communicatorConfig(config('tars.deploy_cfg'));
-                $cservent = new \App\Tars\cservant\PHPTest\LumenTars\tarsObj\TestTafServiceServant($config);
-                return $cservent->test();
-            } catch (\Throwable $e) {
-                return $e->getMessage();
-            }
-        });
+    $router->get('/tars', function () {
+        try {
+            $config = \Lxj\Laravel\Tars\Config::communicatorConfig(config('tars.deploy_cfg'));
+            $cservent = new \App\Tars\cservant\PHPTest\LumenTars\tarsObj\TestTafServiceServant($config);
+            return $cservent->test();
+        } catch (\Throwable $e) {
+            return $e->getMessage();
+        }
     });
 });
